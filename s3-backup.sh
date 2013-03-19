1c5cd793511c706804938eca729eda009e3953f3
Åsmund Grammeltvedt <asmundg@big-oil.org>
2013-03-19 20:11:10 +0100

    Copy .encfs6.xml to the right place
    We can't write it to the reverse-mounted encfs directory,
    so we place it directly in the s3 bucket instead.

--- s3-backup.sh
+++ s3-backup.sh
@@ -6,8 +6,8 @@ s3fs asmundg-backup -o passwd_file=/root/s3-password $S3DIR || exit 1
 
 for dir in /root/off-site-backup/*; do
     encfs --stdinpass --reverse $dir /tmp/enc/ < $dir/.encfs_password || exit 1
-    cp $dir/.encfs6.xml /tmp/enc/
     rsync -aPh --size-only /tmp/enc/ $S3DIR/$(basename $dir)
+    cp $dir/.encfs6.xml $S3DIR/$(basename $dir)/
     umount /tmp/enc/
 done
 
