0d525094541c30bffe7910383e8b9b62548222f3
Åsmund Grammeltvedt <asmundg@big-oil.org>
2013-11-04 20:55:34 +0100

    Various improvements
     * Make script more crash-safe when manipulating directories
     * Create crypto dir using the correct password when first encountering it
     * In-place rsync for less S3 usage (and assume that s3fs does timestamps correctly now)

--- s3-backup.sh
+++ s3-backup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -x
 
 S3DIR=/mnt/s3-backup
-mkdir -p $S3DIR
+umount /mnt/s3-backup || mkdir -p $S3DIR
+umount /tmp/enc || mkdir -p /tmp/enc
 s3fs asmundg-backup -o passwd_file=/root/s3-password $S3DIR || exit 1
 
 for dir in /root/off-site-backup/*; do
-    encfs --stdinpass --reverse $dir /tmp/enc/ < $dir/.encfs_password || exit 1
-    rsync -aPh --size-only /tmp/enc/ $S3DIR/$(basename $dir)
+    encfs --stdinpass --standard --reverse $(readlink $dir) /tmp/enc/ < $dir/.encfs_password || exit 1
+    rsync --delete --inplace -aPh /tmp/enc/ $S3DIR/$(basename $dir)
     cp $dir/.encfs6.xml $S3DIR/$(basename $dir)/
     umount /tmp/enc/
 done
